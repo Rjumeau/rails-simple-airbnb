9a63bad7320d858744f3962e716a86b3480cc536
Romain Jumeau <romain.jumeau@hotmail.fr>
2021-11-10 18:14:57 +0100

    update but not finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating flats..."
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Une maison à Paris',
+  address: 'Rue de la tour Eiffel',
+  description: 'Une maison avec un jardin',
+  price_per_night: 150,
+  number_of_guests: 10
+)
+
+Flat.create!(
+  name: 'Un appartement à Bordeaux',
+  address: 'Rue de la place de la bourse ',
+  description: 'Un canapé lit et tu te demerdes',
+  price_per_night: 800,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Une cabane dans les alpes',
+  address: "Plaine de l'aigle",
+  description: "Une brindille et des vaches",
+  price_per_night: 0,
+  number_of_guests: 50
+)
+
+puts "Finished!"
